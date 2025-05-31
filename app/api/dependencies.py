bfe00b4976d0080a204ac22fe2dce342260ab20f
Di0Zavr <viktorsergeev08@gmail.com>
2025-05-31 23:05:22 +0300

    fix: change auth dependecy line order

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -6,7 +6,7 @@ security = HTTPBearer()
 
 def get_current_user_id(token: HTTPAuthorizationCredentials = Depends(security)) -> int:
     payload = decode_token(token.credentials)
-    user_id = payload.get("id")
-    if not payload or not user_id:
+    if not payload or not payload.get("id"):
         raise HTTPException(status_code=403, detail="Неверные учетные данные")
+    user_id = payload.get("id")
     return int(user_id)
